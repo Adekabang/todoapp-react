cf194970a68f38a97d568bfb4e5d2e2b8e5decb6
Mohammad Raska <iban.ninja@gmail.com>
2020-07-06 02:50:36 +0700

    create delete button and fix map param name to todo

--- src/Todos.js
+++ src/Todos.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Todos = ({todos}) => {
+const Todos = ({todos, deleteTodo}) => {
   const todoList = todos.length ? (
-    todos.map(todos => {
+    todos.map(todo => {
       return (
-        <div className="collection-item" key={todos.id}>
-          <span>{todos.content}</span>
+        <div className="collection-item" key={todo.id}>
+          <span onClick={() => {deleteTodo(todo.id)}}>{todo.content}</span>
         </div>
       )
     })
